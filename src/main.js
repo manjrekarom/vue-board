f1fd4ccc86c34a523281852ffc5f885d43012e86
Omkar Manjrekar <manjrekarom@gmail.com>
2018-06-27 17:44:06 +0530

    Added eventBus to component root

--- src/main.js
+++ src/main.js
@@ -1,9 +1,23 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueChartKick from 'vue-chartkick'
+import Chart from 'chart.js'
 import App from './App'
 
 Vue.config.productionTip = false
+
+// Adding event bus to all vue instances/ components
+const EventBus = new Vue()
+Object.defineProperties(Vue.prototype, {
+  $bus: {
+    get: function () {
+      return EventBus
+    }
+  }
+})
+
+Vue.use(VueChartKick, {adapter: Chart})
 
 /* eslint-disable no-new */
 new Vue({
