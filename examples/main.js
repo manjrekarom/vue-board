da33b2469d6b3f5b13e6db225a78f09f00e5f2f8
Omkar Manjrekar <manjrekarom@gmail.com>
2019-11-05 18:02:34 +0530

    New vue cli configuration

---
+++ examples/main.js
@@ -0,0 +1,8 @@
+new Vue({
+    el: '#app',
+    data: () => {
+        return {
+            "message": "Hello world!"
+        }
+    },
+})
---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  preset: '@vue/cli-plugin-unit-jest',
+  testMatch: [
+    "<rootDir>/tests/unit/**/*.(test|spec).(js|jsx|ts|tsx)|<rootDir>/**/__tests__/*.(js|jsx|ts|tsx)"
+  ]
+};
